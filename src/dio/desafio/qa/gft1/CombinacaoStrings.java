1a3926314e7e5163a96603b65b5793cdcb0ada77
Maxwell Dantas <mdszdev@gmail.com>
2022-04-03 14:19:03 -0300

    Combinação de Strings

---
+++ src/dio/desafio/qa/gft1/CombinacaoStrings.java
@@ -0,0 +1,32 @@
+package dio.desafio.qa.gft1;
+
+import java.util.Scanner;
+
+public class CombinacaoStrings {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int casos = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= casos; i++) {
+            StringBuilder analise = new StringBuilder();
+            String[] palavras = scan.nextLine().split(" ");
+
+            int maxSize = Math.max(palavras[0].length(), palavras[1].length());
+
+            for (int dados = 0; dados < maxSize; dados++) {
+
+                if (dados < palavras[0].length()) {
+                    analise.append(palavras[0].charAt(dados));
+                }
+
+                if (dados < palavras[1].length()) {
+                    analise.append(palavras[1].charAt(dados));
+                }
+
+            }
+            System.out.println(analise.toString());
+        }
+    }
+}
